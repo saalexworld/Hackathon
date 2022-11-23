40adbcbdf5520fa7515137b70072f70a4fc5acf5
Aleksandr Sidorevich <sidorevichaa@gmail.com>
2022-11-23 13:21:20 +0600

    лёгкий

---
+++ hackathon.py
@@ -0,0 +1,50 @@
+# Hackathon: ТЗ Parsing
+# лёгкое
+
+import requests
+import csv
+from bs4 import BeautifulSoup
+def get_csv_file(dict_):
+    with open('dat.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow([dict_['title'], dict_['price'], dict_['img_']])
+
+def get_html(url):
+    res = requests.get(url).text
+    return res
+
+def get_total_pages(html):
+    soup = BeautifulSoup(html, 'lxml')
+    last_page = int(soup.find('span', class_ = 'vm-page-counter').text.split()[-1])
+    return last_page
+
+def get_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    product_list = soup.find_all('div', class_ = 'row')
+    
+    for product in product_list:
+        title = product.find('span', class_ = 'prouct_name').text
+        price = product.find('span', class_ = 'price').text
+        try:
+            img_ = 'https://enter.kg/computers/noutbuki_bishkek'+ product.find('img').get('src')
+        except:
+            img_ = ''
+        dict_ = {'title': title, 'price': price, 'img_':img_}
+        get_csv_file(dict_)
+
+def main():
+    notebooks_url = 'https://enter.kg/computers/noutbuki_bishkek'
+    html = get_html(notebooks_url)
+    number = get_total_pages(html)
+    for page in range(1, number+1):
+        if page == 1:
+            url = 'https://enter.kg/computers/noutbuki_bishkek'
+        else:
+            url = 'https://enter.kg/computers/noutbuki_bishkek' +f'/results,{(page-1)*100+1}-{(page-1)*100}'
+        get_html(url)
+        get_data(html)
+
+with open('dat.csv', 'w') as file:
+    write = csv.writer(file)
+    write.writerow(['title', 'price', 'image'])
+main()
